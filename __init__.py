bf43af7082ecf7e362444758b6f3cbe79aedc61b
Rene-Michel99 <renemichel856@gmail.com>
2023-06-02 11:15:52 -0300

    Adjusting imports to be able to import common features outside of project

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 from .model import MaskRCNN
+from .Configs import Config
+from .CustomCallbacks import ClearMemory
+from .Dataset import CustomDataset, load_images_dataset
 from .Utils import utilfunctions, visualize
